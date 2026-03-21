34fd780e85e0deaf92d868907febe32b512b7532
nitesh.dev <nitesh.dev@proton.me>
2026-03-21 19:41:28 +0530

    annotations inference with types completed

---
+++ learnings/src/types.ts
@@ -0,0 +1,22 @@
+// types in typescript
+// annotation means -> needs to explaining
+// interface means -> self understood
+
+const username: string = "theniteshdev";
+console.log(username);
+
+var test = "Hmm";
+console.log(test);
+
+// test2 = "Hn Ji";
+// this is not working because "use strict" mode enabled
+
+
+// this is type annotation(batana-parta-hai)
+const socialMedia:string = "twitter";
+const userId:number = 33;
+const hasPremium:boolean = false;
+
+// this is inference(samajhdar)
+const isAdmin = false;
+const fruits = ['mangoes', 'apples', 'grapes'];
